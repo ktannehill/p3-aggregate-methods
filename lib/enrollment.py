3f6a86dc8fa9cce5b67d599ceb504ce68d817de4
Kat Tannehill <katetannehill@gmail.com>
2023-10-27 11:42:09 -0600

    Complete lab

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -15,6 +15,19 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    # aggregate mathod to determine how many courses a student is in
+    def course_count(self):
+        return len(self._enrollments)
+
+    # aggregate method assuming we have a grade attribute to get average of all grades in enrollments
+    def aggregate_average_grade(self):
+      # lets assume the grades are stored in a protected attribute called _grades. 
+      total_grades = sum(self._grades.values())
+      num_courses = len(self._grades)
+      average_grade = total_grades / num_courses
+
+      return average_grade
+
 class Course:
     def __init__(self, title):
 
@@ -42,6 +55,15 @@ class Enrollment:
             type(self).all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
+
+    # aggregate method to determine how many enrollments were done for any day students were enrolled
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
 
     def get_enrollment_date(self):
         return self._enrollment_date
